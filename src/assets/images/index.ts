7b7a97555079c6484bfb604fec84e81c11c2dbc7
Ridwan Ajibola <51078424+rajibola@users.noreply.github.com>
2021-03-03 07:29:11 +0100

    feat: Added images and icons

---
+++ src/assets/images/index.ts
@@ -0,0 +1,9 @@
+export const mask1 = require('./mask1.png');
+
+export const mask = {
+  one: require('./mask1.png'),
+  two: require('./mask2.png'),
+  three: require('./mask3.png'),
+  four: require('./mask4.png'),
+  five: require('./mask5.png'),
+};
